1fa5a0b298a87755b1891255f2f4cfd153e99ed1
Muaz <59676679+MuazShash@users.noreply.github.com>
2025-03-13 03:47:41 -0400

    Integration between object localization and hdmi

---
+++ MeteorDestroyerSystem/MeteorDestroyerSystem.gen/sources_1/bd/hdmi/ip/hdmi_array_parser_0_0/hdmi_array_parser_0_0_stub.v
@@ -0,0 +1,24 @@
+// Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2022.1 (win64) Build 3526262 Mon Apr 18 15:48:16 MDT 2022
+// Date        : Thu Mar 13 00:20:47 2025
+// Host        : DESKTOP-BINRERU running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               c:/Users/Muaz/Downloads/hw/hw.gen/sources_1/bd/hdmi/ip/hdmi_array_parser_0_0/hdmi_array_parser_0_0_stub.v
+// Design      : hdmi_array_parser_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a200tsbg484-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* X_CORE_INFO = "array_parser,Vivado 2022.1" *)
+module hdmi_array_parser_0_0(clk, echo, trig, distance_mm, valid)
+/* synthesis syn_black_box black_box_pad_pin="clk,echo[2:0],trig,distance_mm[47:0],valid" */;
+  input clk;
+  input [2:0]echo;
+  output trig;
+  output [47:0]distance_mm;
+  output valid;
+endmodule
